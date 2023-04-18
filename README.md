0acd8cfd617b64b4fb069515ab2fe8816402a3e3
Kamran Khan <kamrankhan.sde@gmail.com>
2023-04-18 13:30:53 +0530

    Task logger Added

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# Task-Schedular
- 
+# **Task-Schedular**
+
+## **How to Use? ⚙️**
+
+### Task you can perform
+
+| Action     |     | Description                      |
+| ---------- | --- | -------------------------------- |
+| openApp    | --> | To open app at schedule time     |
+| openUrl    | --> | To open url at schedule time     |
+| runCommand | --> | Execute command at schedule time |
+
+You can modify the "schedule_str" field in the task object to set different scheduling intervals:
+
+### **For a one-time task:**
+
+```json
+task = {
+    "task_name": "One-time task",
+    "Action": "RunCommand",
+    "args": "echo 'Hello, world!'",
+    "schedule": "2023-04-19 10:30:00"  # Run on April 19th, 2023 at 10:30 AM
+}
+```
+
+### **For a daily task:**
+
+```json
+task = {
+    "task_name": "Daily task",
+    "Action": "RunCommand",
+    "args": "echo 'Good morning!'",
+    "schedule": "08:00"  # Run every day at 8:00 AM
+}
+```
+
+### **For a weekly task:**
+
+```json
+task = {
+    "task_name": "Weekly task",
+    "Action": "RunCommand",
+    "args": "echo 'Happy Monday!'",
+    "schedule": "monday 09:30"  # Run every Monday at 9:30 AM
+}
+```
+
+### **For a monthly task:**
+
+```json
+task = {
+    "task_name": "Monthly task",
+    "Action": "RunCommand",
+    "args": "echo 'Happy first of the month!'",
+    "schedule": "1 12:00"  # Run on the 1st day of every month at noon
+}
+```
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# import csv
 import json
 import os
 import webbrowser
@@ -7,40 +6,34 @@ import subprocess
 import time
 import logging
 
+# Set up logging
+logging.basicConfig(filename='tasks.log', level=logging.INFO,
+                    format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-def open_app(args):
+
+def openApp(args):
     try:
-        subprocess.call([args])
+        path = args
+        subprocess.call([path])
+        logging.info(f'Opened app "{args}"')
     except FileNotFoundError:
-        print(f'Could not find the file "{args}"')
+        logging.error(f'Could not open app "{args}"')
 
 
-def open_url(args):
+def openUrl(args):
     try:
         webbrowser.open(args)
+        logging.info(f'Opened URL "{args}"')
     except Exception as e:
-        print(f'Could not open URL "{args}"')
+        logging.error(f'Could not open URL "{args}": {e}')
 
 
-def run_command(args):
-    os.system(args)
-
-
-# def schedule_tasks():
-#    with open('tasks.csv') as f:
-#        reader = csv.DictReader(f)
-#        for row in reader:
-#            task_name = row['task_name']
-#            function_name = row['function']
-#            args = row['args']
-#            schedule_str = row['schedule']
-#
-#            # Get the function object based on its name
-#            function = globals().get(function_name)
-#
-#            # Schedule the task
-#            schedule.every().day.at(schedule_str).do(function, args)
-#            print(f'Scheduled task "{task_name}" to run at {schedule_str}')
+def runCommand(args):
+    try:
+        os.system(args)
+        logging.info(f'Executed command "{args}"')
+    except Exception as e:
+        logging.error(f'Could not execute command "{args}": {e}')
 
 
 def schedule_tasks():
@@ -48,12 +41,12 @@ def schedule_tasks():
         tasks = json.load(f)
         for task in tasks:
             task_name = task['task_name']
-            function_name = task['function']
+            Action = task['Action']
             args = task['args']
             schedule_str = task['schedule']
 
             # Get the function object based on its name
-            function = globals().get(function_name)
+            function = globals().get(Action)
 
             # Schedule the task
             try:
